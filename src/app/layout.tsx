1dc98c8b2897112ef21ce08664d4995f1c3733e4
João Victor Lourenço Barbosa Bernardo Diniz <87540912+joaovictorlourenco@users.noreply.github.com>
2023-07-04 15:19:20 -0300

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from 'react'
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="pt-br">
+      <body>{children}</body>
+    </html>
+  )
+}
